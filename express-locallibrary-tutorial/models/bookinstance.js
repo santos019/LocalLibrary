ad8dbda09997ead2b3224798e55cbccf564dd645
santos019 <santos019@naver.com>
2022-06-15 18:17:29 +0900

    dev : part5

--- express-locallibrary-tutorial/models/bookinstance.js
+++ express-locallibrary-tutorial/models/bookinstance.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+const { DateTime } = require("luxon");
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema(
@@ -18,5 +18,10 @@ BookInstanceSchema
   return '/catalog/bookinstance/' + this._id;
 });
 
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
 //Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
